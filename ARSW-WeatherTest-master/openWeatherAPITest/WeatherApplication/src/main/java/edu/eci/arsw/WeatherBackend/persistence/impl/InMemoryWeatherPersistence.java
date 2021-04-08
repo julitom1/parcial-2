eaf9fa774491ce6cd8f030bfd4deebcaf29ceb00
Yarit Villalobos <yarit.villalobos@mail.escuelaing.edu.co>
2021-04-08 13:33:14 -0500

    agregando el tiempo

--- ARSW-WeatherTest-master/openWeatherAPITest/WeatherApplication/src/main/java/edu/eci/arsw/WeatherBackend/persistence/impl/InMemoryWeatherPersistence.java
+++ ARSW-WeatherTest-master/openWeatherAPITest/WeatherApplication/src/main/java/edu/eci/arsw/WeatherBackend/persistence/impl/InMemoryWeatherPersistence.java
@@ -4,6 +4,10 @@ package edu.eci.arsw.WeatherBackend.persistence.impl;
 import edu.eci.arsw.WeatherBackend.model.*;
 import java.util.Map;
 import java.util.Set;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Service;
 
@@ -16,15 +20,32 @@ import edu.eci.arsw.WeatherBackend.persistence.*;
 
 @Service
 public class InMemoryWeatherPersistence implements WeatherPersistence{
-
+	//private ArrayList<String> ciudades=new ArrayList<>();
+	private ArrayList<Timer> timers=new ArrayList<>();
     private final ConcurrentHashMap<String,Ciudad> dic=new ConcurrentHashMap<>();
 
  
     @Override
 	public void postDatos(Ciudad ciudad) {
 
-    	
-		dic.put(ciudad.getCiudad(),ciudad);
+    	if(!dic.containsKey(ciudad)){
+			dic.put(ciudad.getCiudad(),ciudad);
+			//Eliminacion cada 5 minutos
+			Timer timer=new Timer(300000,new ActionListener() {
+				@Override
+	            public void actionPerformed(ActionEvent e) {
+	                System.out.println("hola");
+					dic.remove(ciudad.getCiudad());
+	                timers.get(0).stop();
+	                timers.remove(timers.get(0));
+	                
+	            }
+			});
+			
+			timer.start();
+			timers.add(timer);
+		
+    	}
 
 	}
 
@@ -38,4 +59,6 @@ public class InMemoryWeatherPersistence implements WeatherPersistence{
 		return null;
 		
 	}
+	
+	
 }
